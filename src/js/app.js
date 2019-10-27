103cadda0691a3de8947906fa7f34a7127fdd667
Олег <oleg.bordunov@nova-test.ru>
2019-10-27 15:39:37 +0300

    html + images + config split

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-import '../scss/app.scss';
-
 import documentReady from './utils/documentReady';
 
 documentReady(() => {
---
+++ webpack.base.conf.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist')
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    path: PATHS.dist,
+    filename: 'js/[name].bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          publicPath: '../img/',
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development'
+            }
+          },
+          'css-loader',
+          'sass-loader'
+          //@todo postcss
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      ignoreOrder: true
+    }),
+    new HtmlWebpackPlugin({
+      template: `${PATHS.src}/index.html`,
+      filename: './index.html',
+      inject: false,
+      minify: false
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: `${PATHS.src}/img`,
+        to: `${PATHS.dist}/img`,
+        ignore: ['.gitkeep']
+      },
+      {
+        from: `${PATHS.src}/static`,
+        to: `${PATHS.dist}`
+      }
+    ])
+  ]
+};
