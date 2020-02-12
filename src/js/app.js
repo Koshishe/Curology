f7dc67f22e1b0c2d9f46dcf2e1451c37a1996b22
Oleg Bordunov <oleg.bordunov@nova-test.ru>
2020-02-12 11:40:59 +0300

    dispatcher and preloader

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import documentReady from './utils/documentReady';
 import documentLoaded from './utils/documentLoaded';
+import sitePreloader from './modules/sitePreloader';
 import lazyload from './modules/lazyload';
 
 documentReady(() => {
+  sitePreloader.init();
   lazyload.init();
 });
 
--- src/js/utils/EventEmitter.js
+++ src/js/utils/EventEmitter.js
@@ -1,22 +1,88 @@
+/* eslint-disable */
 export default class EventEmitter {
   constructor() {
-    this.handlers = [];
+    this._handlers = {
+      all: [],
+    };
+    this._frozen = false;
   }
 
-  dispatch(event) {
-    this.handlers.forEach((handler) => {
-      handler(event);
-    });
+  dispatch(channel, event) {
+    if (!event) {
+      event = channel;
+      channel = 'all';
+    }
+
+    if (event && event.type.indexOf(':')) {
+      channel = event.type.split(':')[0];
+    }
+
+    if (!Object.prototype.hasOwnProperty.call(this._handlers, channel)) {
+      this._handlers[channel] = [];
+    }
+
+    this._frozen = true;
+
+    this._handlers[channel].forEach((handler) => handler(event));
+
+    if (channel !== 'all') {
+      this._handlers.all.forEach((handler) => handler(event));
+    }
+
+    this._frozen = false;
   }
 
-  subscribe(handler) {
-    if (this.handlers.indexOf(handler) === -1
-      && typeof handler === 'function') {
-      this.handlers.push(handler);
+  subscribe(channel, handler) {
+    if (!handler) {
+      handler = channel;
+      channel = 'all';
+    }
+
+    if (this._frozen) {
+      console.error('trying to subscribe to EventEmitter while dispatch is working');
+    }
+
+    if (typeof handler !== 'function') {
+      console.error('handler has to be a function');
+      return;
+    }
+
+    if (!Object.prototype.hasOwnProperty.call(this._handlers, channel)) {
+      this._handlers[channel] = [];
+    }
+
+    if (this._handlers[channel].indexOf(handler) === -1) {
+      this._handlers[channel].push(handler);
+    } else {
+      console.error('handler already set');
     }
   }
 
-  unsubscribe(handler) {
-    this.handlers = this.handlers.filter((h) => h !== handler);
+  unsubscribe(channel, handler) {
+    if (!handler) {
+      handler = channel;
+      channel = 'all';
+    }
+
+    if (this._frozen) {
+      console.error('trying to unsubscribe from EventEmitter while dispatch is working');
+    }
+
+    if (typeof handler !== 'function') {
+      console.error('handler has to be a function');
+    }
+
+    if (!this._handlers[channel]) {
+      console.error(`channel ${ channel } does not exist`);
+      return;
+    }
+
+    if (this._handlers[channel].indexOf(handler) === -1) {
+      console.log(handler);
+      console.error('trying to unsubscribe unexisting handler');
+      return;
+    }
+
+    this._handlers[channel] = this._handlers[channel].filter((h) => h !== handler);
   }
 }
