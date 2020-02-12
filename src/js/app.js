96be8c259d029c99427b20d1f1ca68c5c849b2c1
Oleg Bordunov <oleg.bordunov@nova-test.ru>
2020-02-12 17:32:42 +0300

    css vars module

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import documentReady from './utils/documentReady';
 import documentLoaded from './utils/documentLoaded';
 import sitePreloader from './modules/sitePreloader';
 import resize from './modules/resize';
+import cssVars from './modules/cssVars';
 import lazyload from './modules/lazyload';
 
 documentReady(() => {
   sitePreloader.init();
   resize.init();
+  cssVars.init();
   lazyload.init();
 });
 
