8b1b940a2028614877879429f0746c6f70619a96
Oleg Bordunov <oleg.bordunov@nova-test.ru>
2020-02-12 17:28:47 +0300

    resize module

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 import documentReady from './utils/documentReady';
 import documentLoaded from './utils/documentLoaded';
 import sitePreloader from './modules/sitePreloader';
+import resize from './modules/resize';
 import lazyload from './modules/lazyload';
 
 documentReady(() => {
   sitePreloader.init();
+  resize.init();
   lazyload.init();
 });
 
